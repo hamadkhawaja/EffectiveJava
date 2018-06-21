efeb54de8f9c58b671a698cf64d0312084802e35
Hamad Khawaja <hamad.jillani@gmail.com>
2018-06-21 01:16:30 -0700

    first commit

---
+++ test/com/HelloWorld/HelloWorldTest.java
@@ -0,0 +1,34 @@
+package com.HelloWorld;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+        System.setErr(System.err);
+    }
+
+    @Test
+    public void helloWorldTest() {
+        HelloWorld.main(null);
+        assertTrue(outContent.toString().startsWith("Hello World!"));
+    }
+}
